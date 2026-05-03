6d28572570090b8ae994874c78249f23a63785d4
fre-denni <fredenni.git@gmail.com>
2026-05-03 22:55:01 +0200

    added components file

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,13 @@
 <script>
+  import Header from "./lib/components/header/Header.svelte";
+  import Feed from "./lib/components/Feed.svelte";
+  import Modal from "./lib/components/modal/Modal.svelte";
+  import Footer from "./lib/components/footer/Footer.svelte";
 </script>
 
 <h1>Work in progress!</h1>
+
+<Header />
+<Feed />
+<Footer />
+<Modal />
