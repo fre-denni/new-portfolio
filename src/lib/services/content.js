e273e43e6b14cbce45f6d99ada2313328928f0ff
fre-denni <fredenni.git@gmail.com>
2026-05-08 16:57:56 +0200

    added language control e content

--- src/lib/services/content.js
+++ src/lib/services/content.js
@@ -2,7 +2,7 @@ import archieml from "archieml";
 //stringa grezza
 import raw from "../../content/content.aml?raw";
 
-//esporta e
+//esporta e fai parsing con archieml
 export const loadContent = () => {
-  return archieml.load(raw); //capire se funziona
+  return archieml.load(raw);
 };
--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,2 +1,5 @@
 //dark and light mode
+
 //language
+import { writable } from "svelte/store";
+export const lang = writable("it");
