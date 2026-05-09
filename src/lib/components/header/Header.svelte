b65d2b948a8608e1e1c6fb249bcc811d3819ffd9
fre-denni <fredenni.git@gmail.com>
2026-05-09 14:40:57 +0200

    Added prop test

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Toggle from "./Toggle.svelte";
+  //props
+  let { payoffContent, url, action } = $props();
+</script>
+
+<header>
+  <Toggle />
+  <h1 class="payoff-title">Federico Denni</h1>
+  <p class="payoff-text">
+    {payoffContent}
+    <span><a href={url}>{action}</a></span>
+  </p>
+</header>
+
+<style>
+  .payoff-title {
+    font-family: var(--title);
+  }
+
+  .payoff-text {
+    white-space: pre-wrap;
+  }
+  /***Mobile**/
+  /***Desktop**/
+</style>
