d695c6e2c96d35cb28758b6511148b84137fb642
fre-denni <fredenni.git@gmail.com>
2026-05-17 16:12:29 +0200

    Added button component

--- src/lib/components/atoms/Btn.svelte
+++ src/lib/components/atoms/Btn.svelte
@@ -1,5 +1,40 @@
 <script>
-  //animation
-  //styling and space
-  //text
+  //props
+  let {
+    text,
+    loading = false,
+    error = false,
+    //{...props} is for onclick event to change feed
+    ...props
+  } = $props();
+
+  //add animation
 </script>
+
+<button
+  class="
+        text-btn-rg
+        {loading ? 'loading' : ''}
+        {error ? 'error' : ''}
+        "
+  {...props}
+>
+  {#if loading}
+    <span class="icon"></span>
+  {:else if error}
+    <span class="icon"></span>
+  {/if}
+  {text}
+</button>
+
+<style>
+  button {
+    background-color: var(--btn-fill);
+    border-color: var(--btn-border);
+  }
+
+  /* .loading {
+  }
+  .error {
+  } */
+</style>
