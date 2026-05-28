ffdd8d472d3dc7eb0ac58ac94c49fd54a86849dd
fre-denni <fredenni.git@gmail.com>
2026-05-28 21:27:04 +0200

    eliminated as not needed anymore

--- src/lib/components/atoms/Icon.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  //used in the config for setup a custom icon compiler
-  //unplugin-icons library
-  let { content, ...props } = $props();
-</script>
-
-<svg {...props}>
-  {@html content}
-</svg>
